293aa472ca34538a9e8abe6aab354863acf31877
logonrm <logonrm@fiap.com.br>
2017-11-25 15:03:24 -0200

    Primeiro Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PessoasProvider } from '../../providers/pessoas/pessoas'
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [PessoasProvider]
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public pessoas:any;
 
+  constructor(public navCtrl: NavController,
+              public PessoasProvider: PessoasProvider) {
+
+             this.loadPessoas();
+
+  }
+
+  loadPessoas ()
+  {
+    this.PessoasProvider.load().then(data => {
+      this.pessoas = data;
+    });
   }
 
 }
